f6bd7b6270bb338f71ea3ccd11680c317a732a79
Adrien Matricon <adrien.matricon@polytechnique.org>
2026-07-18 22:47:59 +0200

    Use piano samples from Salamander Grand Piano V3 through @audio-samples/piano-velocity1

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,19 @@
 import { defineConfig } from "vite";
+import copy from "rollup-plugin-copy";
 
 export default defineConfig({
-    base: "./"
+    base: "./",
+    plugins: [
+        copy({
+            targets: [
+                {
+                    src: "node_modules/@audio-samples/piano-velocity3/audio/*.ogg",
+                    dest: "dist/piano",
+                    rename: (name, extension) =>
+                        `${name.replace("#", "sharp")}.${extension}`
+                }
+            ],
+            hook: "writeBundle"
+        })
+    ]
 });
